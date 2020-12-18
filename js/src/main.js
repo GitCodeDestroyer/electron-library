2faab7e790f070aeb369bddf2f12eb1242969e94
GitCodeDestroyer <oybek.odili@gmail.com>
2020-12-18 16:29:06 +0500

    first commit

---
+++ js/src/main.js
@@ -0,0 +1,12 @@
+new WOW().init();
+
+$(".content").niceScroll({
+    cursorcolor: "#6969E5",
+    cursorwidth: "10px",
+    cursorborderradius: "10px",
+    cursorborder: "none",
+    zindex: 1000000,
+    mousescrollstep: 40,
+    bouncescroll: !1,
+    cursoropacitymin: 1
+});
